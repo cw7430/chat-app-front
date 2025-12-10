a526f72184d0a6ca286cd7ee1b92b47564d57428
cw7430 <cw282@naver.com>
2025-12-10 17:42:31 +0900

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import os from 'os';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -8,11 +9,17 @@ const eslintConfig = defineConfig([
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
+  {
+    rules: {
+      'linebreak-style': ['error', os.EOL === '\r\n' ? 'windows' : 'unix'],
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    },
+  },
 ]);
 
 export default eslintConfig;
